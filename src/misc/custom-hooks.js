25f8ce6022301c28da50197c9c30efaec6ab8a33
anihazard <lou.ani.1405@gmail.com>
2021-10-12 19:26:00 +0530

    using custom hooks for starred shows

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,15 +1,36 @@
 import { useReducer, useEffect } from 'react';
 
+function showsReducer(prevState, action) {
+  switch (action.type) {
+    case 'ADD': {
+      return [...prevState, action.showId];
+    }
+
+    case 'REMOVE': {
+      return prevState.filter(showId => showId !== action.showId);
+    }
+
+    default:
+      return prevState;
+  }
+}
+
 function usePersistedReducer(reducer, initialState, key) {
+  // custom wrapper-hook for useReducer
   const [state, dispatch] = useReducer(reducer, initialState, initial => {
     const persisted = localStorage.getItem(key);
 
-    return persisted ? JSON.parse(persisted) : initial;
+    return persisted ? JSON.parse(persisted) : initial; // so that the value persists even after reloading the page
   });
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
+    localStorage.setItem(key, JSON.stringify(state)); // everytime state changes the new id gets added to the state
   }, [state, key]);
 
   return [state, dispatch];
+}
+
+export function useShows(key = 'shows') {
+  // a wrapper around usePersistedReducer
+  return usePersistedReducer(showsReducer, [], key);
 }
